7cad6fe3ed185763d12eb8d479ca3f84d8ce3bbe
unknown <stelios05@gmail.com>
2014-11-25 22:55:40 +0000

    de.bat does not work correctly

--- provision.sh
+++ provision.sh
@@ -1,2 +1,8 @@
-apt-get install -y ubuntu-desktop
-apt-get install -y eclipse
+apt-get update
+apt-get install --no-install-recommends -y ubuntu-desktop
+#sudo apt-get install -y --no-install-recommends gnome-shell
+#apt-get install -y --no-install-recommends xubuntu-desktop
+apt-get install -y --no-install-recommends git
+apt-get install -y --no-install-recommends eclipse
+#reboot
+#apt-get install -y eclipse
