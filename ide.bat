5173285df0f7de9868e49c86c94690d345b67a91
unknown <stelios05@gmail.com>
2014-12-26 10:52:29 +0000

    default user is logged in. script waits for enter in order to bring the box down.

--- ide.bat
+++ ide.bat
@@ -1,1 +1,4 @@
 vagrant up
+echo press enter to bring it down
+pause
+vagrant halt
--- provision.sh
+++ provision.sh
@@ -1,8 +1,24 @@
 apt-get update
 apt-get install --no-install-recommends -y ubuntu-desktop
-#sudo apt-get install -y --no-install-recommends gnome-shell
-#apt-get install -y --no-install-recommends xubuntu-desktop
+
 apt-get install -y --no-install-recommends git
 apt-get install -y --no-install-recommends eclipse
-#reboot
-#apt-get install -y eclipse
+apt-get install -y --no-install-recommends maven
+
+
+if [[ ! -f /etc/lightdm/lightdm.conf ]] 
+then
+	echo seting up autologin
+	cp /vagrant/conf/lightdm.conf /etc/lightdm/lightdm.conf
+	chmod 644 /etc/lightdm/lightdm.conf
+else 
+	echo /etc/lightdm/lightdm.conf exists will not replace it
+fi
+grep 192.168.0.7 /etc/hosts || echo 192.168.0.7 server-lan >> /etc/hosts
+
+#I dont know how to change the keyboard System->text entry
+#I don't know how to disable the autolock System->security
+#I don't know how to make it have multiple desktops
+
+reboot
+
