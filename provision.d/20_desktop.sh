36d83405b5098d984c6d3c764380ff31f95e5d52
stelios <stelios05@gmail.com>
2017-04-23 17:34:30 +0100

    autologin

--- provision.d/20_desktop.sh
+++ provision.d/20_desktop.sh
@@ -1,1 +1,6 @@
 sudo apt-get install -y --no-install-recommends ubuntu-desktop
+
+sudo mkdir -p /etc/lightdm/lightdm.conf.d
+sudo bash -c "echo '[SeatDefaults]
+autologin-user=ubuntu
+' > /etc/lightdm/lightdm.conf.d/vagrant_autologin.conf"
